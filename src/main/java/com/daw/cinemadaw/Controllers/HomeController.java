232c12cada289f791a99152cd3437f48226db4fc
Ivan Gonzalez <ivgo700@vidalibarraquer.net>
2026-03-02 10:05:52 +0100

    2 mar, 10:00

--- src/main/java/com/daw/cinemadaw/Controllers/HomeController.java
+++ src/main/java/com/daw/cinemadaw/Controllers/HomeController.java
@@ -1,9 +1,12 @@
 package com.daw.cinemadaw.Controllers;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.daw.cinemadaw.domain.cinema.Cinema;
 import com.daw.cinemadaw.repository.CinemaRepository;
@@ -27,5 +30,29 @@ public class HomeController {
         List<Cinema> cinemes = cinemaRepository.findAll();
         model.addAttribute("llista", cinemes);
         return "cinemes";
+    }
+
+    //detalls del cinema
+    @GetMapping("/cinema/{id}")
+    public String detall(@PathVariable Long id, Model model) {
+
+    Optional<Cinema> optional = cinemaRepository.findById(id);
+
+    if (optional.isPresent()) {
+        Cinema cinema = optional.get();
+        model.addAttribute("cinema", cinema);
+        return "detall-cinema";
+    }
+
+        return "redirect:/";
+
+    }
+
+    //Esborrar cinema
+    @GetMapping("/cinema/delete/{id}")
+    public void esborrar(@PathVariable Long id, Model model) {
+
+        cinemaRepository.deleteById(id);
+
     }
 }
